2fdc7930c8d7af5c8aa17466c2885e2584f58900
Karolina Acosta <dianakarolinaacostaromero17@gmail.com>
2023-10-05 17:58:57 -0500

    final

--- proyectonpm.js
+++ proyectonpm.js
@@ -1,93 +1,145 @@
+const fs = require("fs");
 const readline = require("readline");
-const leerDatos = readline.createInterface([
-  {
-    input: process.stadin,
-    out: process.stdout,
-  },
-]);
 
-const listaDeTareas = [
+const leerDatos = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let listaDeTareas = [
   {
     indicador: 1,
-    Descripcion: "Hacer aseo",
-    Estado: "Completado",
+    descripcion: "Hacer  aseo a la casa",
+    estado: "Completado",
   },
   {
     indicador: 2,
-    Descripcion: "Ir al supermercado",
-    Estado: "Completado",
+    descripcion: "Ir al supermercado",
+    estado: "Completado",
   },
   {
     indicador: 3,
-    Descripcion: "Organizar mercado",
-    Estado: " Completado",
+    descripcion: "Organizar mercado",
+    estado: " Completado",
   },
   {
     indicador: 4,
-    Descripcion: "Hacer el almuerzo",
-    Estado: "Incompleto",
+    descripcion: "Hacer el almuerzo",
+    estado: "Incompleto",
   },
   {
     indicador: 5,
-    Descripcion: "Sacar los perros al parque",
-    Estado: "Incompleto",
+    descripcion: "Sacar los perros al parque",
+    estado: "Incompleto",
   },
 ];
 
-function añadirTarea(indicador, Descripcion, Estado) {
-  const nuevaTarea = {
-    indicador: indicador,
-    Descripcion: Descripcion,
-    Estado: Estado,
-  };
-  listaDeTareas.push(nuevaTarea);
-  console.log(`tarea" ${(indicador, Descripcion, Estado)} "agregado.`);
+function menu() {
+  return `
+  seleccionar una de las opciones:
+  1. Mirar lista de Tareas
+  2. Agregar tarea
+  3. Marcar tarea completa
+  4. Actualizar Tarea
+  5. Eliminar tarea
+  `;
 }
-
-function borrarTarea(indicador, Descripcion, Estado) {
-  const eliminartarea = {
-    indicador: indicador,
-    Descripcion: Descripcion,
-    Estado: Descripcion,
-  };
-  if (eliminartarea !== -1) {
-    listaDeTareas.splice(eliminartarea, 1);
-    console.log(`tarea " ${Descripcion}"eliminado.`);
-  } else {
-    console.log(
-      `El tarea "${
-        (indicador, Descripcion, Estado)
-      }" no se encontro en la lista`
-    );
-  }
-}
-
-function imprimirlistaDeTareas() {
-  console.log("LISTA DE tareaS:");
+function imprimirlistaDeTareas(listaDeTareas) {
+  console.log("LISTA DE TAREAS:");
   for (const tarea of listaDeTareas) {
     console.log(tarea);
   }
+  mostrarMenu();
 }
 
-function actualizarTarea(nuevoIndicador, nuevoDescripcion, nuevoEstado) {
+function añadirTarea(indicador, descripcion, estado) {
+  const nuevaTarea = {
+    indicador: indicador,
+    descripcion: descripcion,
+    estado: estado,
+  };
+  listaDeTareas.push(nuevaTarea);
+  console.log(`tarea" ${(indicador, descripcion, estado)} "agregado.`);
+  mostrarMenu();
+}
+
+function marcarComoCompletada(indice, tareas) {
+  if (indice >= 0 && indice < tareas.length) {
+    tareas[indice - 1].estado = "completado";
+    console.log(`Tarea "${listaDeTareas[indice].descripcion}" completada.`);
+  } else {
+    console.log("Índice de tarea no válido.");
+  }
+  mostrarMenu();
+}
+
+function actualizarTarea(indicador, nuevaDescripcion, nuevoEstado) {
   const tarea = listaDeTareas.find((tarea) => tarea.indicador === indicador);
   if (tarea) {
-    tarea.Descripcion = nuevoDescripcion;
-    tarea.Estado = nuevoEstado;
-    console.log(
-      `Tarea actualizada: ${tarea.Descripcion}, ${tarea.Estado},${tarea.Numero}`
-    );
+    tarea.descripcion = nuevaDescripcion;
+    tarea.estado = nuevoEstado;
+    console.log(`Tarea actualizada: ${nuevaDescripcion}, ${nuevoEstado}`);
   } else {
-    console.log("La tarea no se encontró en la lista de Tareas.");
+    console.log(`Tarea no encontrada en la lista`);
   }
+  mostrarMenu();
 }
 
-añadirTarea(6, "Hacer Ejercicio", "Incompleta");
-imprimirlistaDeTareas();
+function eliminarTarea(indice) {
+  if (indice >= 0 && indice < listaDeTareas.length) {
+    const tareaEliminada = listaDeTareas.splice(indice, 1);
+    console.log("Tarea eliminada con éxito:", tareaEliminada[0].descripcion);
+  } else {
+    console.log("Índice fuera de rango. La tarea no se pudo eliminar.");
+  }
+  mostrarMenu();
+}
 
-borrarTarea(1, "Hacer aseo", "Completado");
-imprimirlistaDeTareas();
-actualizarTarea(4, "Hacer almuerzo", "Completo");
-console.log(listaDeTareas);
-
-imprimirlistaDeTareas();
+function mostrarMenu() {
+  console.log(menu());
+  leerDatos.question("Ingrese la opción deseada:", (opcion) => {
+    switch (opcion) {
+      case "1":
+        imprimirlistaDeTareas(listaDeTareas);
+        break;
+      case "2":
+        leerDatos.question("Introduca la indicador", (indicador) => {
+          leerDatos.question("Introducir descripcion", (descripcion) => {
+            leerDatos.question("Introducir estado", (estado) => {
+              añadirTarea(indicador, descripcion, estado);
+            });
+          });
+        });
+        break;
+      case "3":
+        leerDatos.question(
+          "Escribe el indice de la tarea para marcarcarla como completada",
+          (indicador) => {
+            marcarComoCompletada(indicador, listaDeTareas);
+          }
+        );
+        break;
+      case "4":
+        leerDatos.question("Introduca la indicador", (indicador) => {
+          leerDatos.question(
+            "Introducir la nueva descripcion",
+            (nuevaDescripcion) => {
+              leerDatos.question("Introducir estado", (nuevoEstado) => {
+                actualizarTarea(
+                  parseInt(indicador),
+                  nuevaDescripcion,
+                  nuevoEstado
+                );
+              });
+            }
+          );
+        });
+        break;
+      case "5":
+        leerDatos.question("Indica que tarea deseas eliminar", (indicador) => {
+          eliminarTarea(parseInt(indicador));
+        });
+    }
+  });
+}
+mostrarMenu();
