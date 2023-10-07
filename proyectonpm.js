732e7b07b6df8c6e37a1aff1b81cd56b92bf832b
Karolina Acosta <dianakarolinaacostaromero17@gmail.com>
2023-10-06 21:59:43 -0500

    promesas

--- proyectonpm.js
+++ proyectonpm.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const readline = require("readline");
 
 const leerDatos = readline.createInterface({
@@ -9,7 +10,7 @@ const leerDatos = readline.createInterface({
 let listaDeTareas = [
   {
     indicador: 1,
-    descripcion: "Hacer  aseo a la casa",
+    descripcion: "Hacer aseo a la casa",
     estado: "Completado",
   },
   {
@@ -44,6 +45,7 @@ function menu() {
   5. Eliminar tarea
   `;
 }
+
 function imprimirlistaDeTareas(listaDeTareas) {
   console.log("LISTA DE TAREAS:");
   for (const tarea of listaDeTareas) {
@@ -53,93 +55,164 @@ function imprimirlistaDeTareas(listaDeTareas) {
 }
 
 function añadirTarea(indicador, descripcion, estado) {
-  const nuevaTarea = {
-    indicador: indicador,
-    descripcion: descripcion,
-    estado: estado,
-  };
-  listaDeTareas.push(nuevaTarea);
-  console.log(`tarea" ${(indicador, descripcion, estado)} "agregado.`);
-  mostrarMenu();
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const nuevaTarea = {
+        indicador: indicador,
+        descripcion: descripcion,
+        estado: estado,
+      };
+      listaDeTareas.push(nuevaTarea);
+      console.log(`tarea "${descripcion}" agregada.`);
+      mostrarMenu();
+      resolve();
+    }, 2000);
+  });
 }
 
-function marcarComoCompletada(indice, tareas) {
-  if (indice >= 0 && indice < tareas.length) {
-    tareas[indice - 1].estado = "completado";
-    console.log(`Tarea "${listaDeTareas[indice].descripcion}" completada.`);
-  } else {
-    console.log("Índice de tarea no válido.");
+async function marcarComoCompletada(indice, tareas) {
+  try {
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (indice >= 1 && indice <= tareas.length) {
+          // Corregir la condición
+          tareas[indice - 1].estado = "completado";
+          console.log(
+            `Tarea "${listaDeTareas[indice - 1].descripcion}" completada.`
+          );
+          resolve();
+        } else {
+          console.log("Índice de tarea no válido.");
+          reject("Índice de tarea no válido.");
+        }
+        mostrarMenu();
+      }, 2000);
+    });
+  } catch (error) {
+    console.error("Error al marcar la tarea como completada:", error);
+    mostrarMenu();
   }
-  mostrarMenu();
 }
 
 function actualizarTarea(indicador, nuevaDescripcion, nuevoEstado) {
-  const tarea = listaDeTareas.find((tarea) => tarea.indicador === indicador);
-  if (tarea) {
-    tarea.descripcion = nuevaDescripcion;
-    tarea.estado = nuevoEstado;
-    console.log(`Tarea actualizada: ${nuevaDescripcion}, ${nuevoEstado}`);
-  } else {
-    console.log(`Tarea no encontrada en la lista`);
-  }
-  mostrarMenu();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const tarea = listaDeTareas.find(
+        (tarea) => tarea.indicador === indicador
+      );
+      if (tarea) {
+        tarea.descripcion = nuevaDescripcion;
+        tarea.estado = nuevoEstado;
+        console.log(`Tarea actualizada: ${nuevaDescripcion}, ${nuevoEstado}`);
+        resolve();
+      } else {
+        console.log(`Tarea no encontrada en la lista`);
+        reject();
+      }
+      mostrarMenu();
+    }, 2000);
+  });
 }
 
 function eliminarTarea(indice) {
-  if (indice >= 0 && indice < listaDeTareas.length) {
-    const tareaEliminada = listaDeTareas.splice(indice, 1);
-    console.log("Tarea eliminada con éxito:", tareaEliminada[0].descripcion);
-  } else {
-    console.log("Índice fuera de rango. La tarea no se pudo eliminar.");
-  }
-  mostrarMenu();
-}
-
-function mostrarMenu() {
-  console.log(menu());
-  leerDatos.question("Ingrese la opción deseada:", (opcion) => {
-    switch (opcion) {
-      case "1":
-        imprimirlistaDeTareas(listaDeTareas);
-        break;
-      case "2":
-        leerDatos.question("Introduca la indicador", (indicador) => {
-          leerDatos.question("Introducir descripcion", (descripcion) => {
-            leerDatos.question("Introducir estado", (estado) => {
-              añadirTarea(indicador, descripcion, estado);
-            });
-          });
-        });
-        break;
-      case "3":
-        leerDatos.question(
-          "Escribe el indice de la tarea para marcarcarla como completada",
-          (indicador) => {
-            marcarComoCompletada(indicador, listaDeTareas);
-          }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (indice >= 1 && indice <= listaDeTareas.length) {
+        // Corregir la condición
+        const tareaEliminada = listaDeTareas.splice(indice - 1, 1); // Restar 1 al índice
+        console.log(
+          "Tarea eliminada con éxito:",
+          tareaEliminada[0].descripcion
         );
-        break;
-      case "4":
-        leerDatos.question("Introduca la indicador", (indicador) => {
-          leerDatos.question(
-            "Introducir la nueva descripcion",
-            (nuevaDescripcion) => {
-              leerDatos.question("Introducir estado", (nuevoEstado) => {
-                actualizarTarea(
-                  parseInt(indicador),
-                  nuevaDescripcion,
-                  nuevoEstado
-                );
-              });
-            }
-          );
-        });
-        break;
-      case "5":
-        leerDatos.question("Indica que tarea deseas eliminar", (indicador) => {
-          eliminarTarea(parseInt(indicador));
-        });
-    }
+        resolve();
+      } else {
+        console.log("Índice fuera de rango. La tarea no se pudo eliminar.");
+        reject("Índice fuera de rango"); // Agregar un mensaje de error y llamar a 'reject'
+      }
+      mostrarMenu();
+    }, 1000);
+  }).catch((error) => {
+    console.error("Error al eliminar la tarea:", error);
+    mostrarMenu();
   });
 }
+
+async function mostrarMenu() {
+  console.log(menu());
+
+  const opcion = await new Promise((resolve) => {
+    leerDatos.question("Ingrese la opción deseada:", (opcion) => {
+      resolve(opcion);
+    });
+  });
+
+  switch (opcion) {
+    case "1":
+      imprimirlistaDeTareas(listaDeTareas);
+      break;
+    case "2":
+      const indicador = await new Promise((resolve) => {
+        leerDatos.question("Introduzca el indicador: ", (indicador) => {
+          resolve(indicador);
+        });
+      });
+      const descripcion = await new Promise((resolve) => {
+        leerDatos.question("Introduzca la descripción: ", (descripcion) => {
+          resolve(descripcion);
+        });
+      });
+      const estado = await new Promise((resolve) => {
+        leerDatos.question("Introduzca el estado: ", (estado) => {
+          resolve(estado);
+        });
+      });
+      await añadirTarea(indicador, descripcion, estado);
+      break;
+    case "3":
+      const indiceCompleta = await new Promise((resolve) => {
+        leerDatos.question(
+          "Escriba el índice de la tarea para marcarla como completada: ",
+          (indice) => {
+            resolve(indice);
+          }
+        );
+      });
+      await marcarComoCompletada(indiceCompleta, listaDeTareas);
+      break;
+    case "4":
+      const indicadorActualizar = await new Promise((resolve) => {
+        leerDatos.question("Introduzca el indicador: ", (indicador) => {
+          resolve(indicador);
+        });
+      });
+      const nuevaDescripcion = await new Promise((resolve) => {
+        leerDatos.question(
+          "Introduzca la nueva descripción: ",
+          (descripcion) => {
+            resolve(descripcion);
+          }
+        );
+      });
+      const nuevoEstado = await new Promise((resolve) => {
+        leerDatos.question("Introduzca el nuevo estado: ", (estado) => {
+          resolve(estado);
+        });
+      });
+      await actualizarTarea(
+        parseInt(indicadorActualizar),
+        nuevaDescripcion,
+        nuevoEstado
+      );
+      break;
+    case "5":
+      const indiceEliminar = await new Promise((resolve) => {
+        leerDatos.question("Indique qué tarea desea eliminar: ", (indice) => {
+          resolve(indice);
+        });
+      });
+      await eliminarTarea(parseInt(indiceEliminar));
+      break;
+  }
+}
+
 mostrarMenu();
