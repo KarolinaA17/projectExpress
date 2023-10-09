e2d2085e549ed9d330ff328ba7793f5f683b538d
Karolina Acosta <dianakarolinaacostaromero17@gmail.com>
2023-10-09 15:18:29 -0500

    Paso 1&2

---
+++ list-view-router.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+Router.param(function (param, Completado) {
+  return function (req, res, next, status) {
+    if (status === Completado) {
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  };
+});
+
+Router.param(`estado`, `Completado`);
+
+Router.get(`/listaDeTareas/:estado`, function (req, res) {
+  res.send("Tareas completadas");
+});
+
+app.use(Router);
